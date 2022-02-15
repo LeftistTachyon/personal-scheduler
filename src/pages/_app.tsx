89f6ce081c011322514f11cafebc8113023ddf31
Gui Guy <jedandeganwang1999@gmail.com>
2022-02-15 00:41:07 -0600

    Pass build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,12 @@
 import { ChakraProvider, Flex, Stack } from "@chakra-ui/react";
 import Footer from "@components/footer";
 import Header from "@components/header";
-import { pageview } from "@lib/gtag";
 import theme from "@styles/theme";
 import { META } from "config";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	const router = useRouter();
-	useEffect(() => {
-		const handleRouteChange = (url: unknown) => {
-			pageview(url);
-		};
-		router.events.on("routeChangeComplete", handleRouteChange);
-		return () => {
-			router.events.off("routeChangeComplete", handleRouteChange);
-		};
-	}, [router.events]);
-
 	return (
 		<>
 			<Head>
