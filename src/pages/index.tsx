c20dabe63d1a3491b7810c7ab090806fb0f4c09a
Gui Guy <jedandeganwang1999@gmail.com>
2022-02-14 22:53:48 -0600

    Made action cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import {
 	Box,
+	BoxProps,
 	Center,
 	Grid,
+	Heading,
 	Icon,
 	SimpleGrid,
 	Stack,
@@ -11,7 +13,8 @@ import {
 } from "@chakra-ui/react";
 import NextChakraLink from "@components/nextChakraLink";
 import React, { cloneElement } from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaEdit, FaGithub } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const shownHours: string[] = [
 	"0:00",
@@ -50,45 +53,19 @@ export default function Home() {
 			spacing={10}
 			justifySelf="stretch"
 			align="stretch"
-			minW="100vw"
+			// minW="100vw"
 			p={10}
 			direction="row"
 		>
-			<GridItem as="button">
+			<GridItem
+				as="button"
+				boxShadow="none"
+				color="transparent"
+				bg="transparent"
+			>
 				<Text>&lt;</Text>
 			</GridItem>
 			<GridItem flex={1} align="stretch" p={5}>
-				{/* <Stack spacing={0}>
-					<Divider flex={1} />
-					{shownHours.map((hour) => (
-						<HStack key={hour} flex={2}>
-							<Divider />
-							<Text as="i">{hour}</Text>
-							<Divider />
-						</HStack>
-					))}
-					<Divider flex={1} />
-				</Stack> */}
-				{/* <Box position="relative" h="100%">
-					{shownHours.map((hour, idx) => (
-						<HStack
-							key={hour}
-							position="absolute"
-							// top={(idx * height) / shownHours.length}
-							top={(100.0 * idx) / shownHours.length + "%"}
-							h={100.0 / shownHours.length + "%"}
-							w="100%"
-						>
-							<Text as="i" alignSelf="center">
-								{hour}
-							</Text>
-							<Divider
-								alignSelf="flex-start"
-								borderColor="black"
-							/>
-						</HStack>
-					))}
-				</Box> */}
 				<Grid
 					templateColumns="auto minmax(0, 1fr)"
 					columnGap={2}
@@ -97,7 +74,7 @@ export default function Home() {
 					{shownHours.map((hour, idx) =>
 						cloneElement(
 							<>
-								<Text textAlign="right">{hour}</Text>
+								<Center textAlign="right">{hour}</Center>
 								<Box
 									borderColor="black"
 									borderTopWidth={1}
@@ -111,11 +88,111 @@ export default function Home() {
 					)}
 				</Grid>
 			</GridItem>
-			<VStack flex={2} align="stretch" spacing={10}>
+			<VStack flex={2} align="stretch" spacing={7}>
 				<Box flex={1}>
 					<SimpleGrid columns={2} spacing={10}>
-						<GridItem></GridItem>
-						<GridItem></GridItem>
+						<GridItem p={5} display="block">
+							<Stack direction="row" float="right">
+								<InsetButton>
+									<Icon as={FaEdit} boxSize={4} />
+								</InsetButton>
+								<InsetButton>
+									<Icon as={MdDelete} boxSize={4} />
+								</InsetButton>
+							</Stack>
+							<Heading size="md">Test item 1</Heading>
+							<Text>
+								This is a lengthy description that only exists
+								to take up an extremely unnecessary amount of
+								space and appear as though that I put many hours
+								of effort into the descriptions of these tasks.
+							</Text>
+						</GridItem>
+						<GridItem p={5} display="block">
+							<Stack direction="row" float="right">
+								<InsetButton>
+									<Icon as={FaEdit} boxSize={4} />
+								</InsetButton>
+								<InsetButton>
+									<Icon as={MdDelete} boxSize={4} />
+								</InsetButton>
+							</Stack>
+							<Heading size="md">Test item 1</Heading>
+							<Text>
+								This is a lengthy description that only exists
+								to take up an extremely unnecessary amount of
+								space and appear as though that I put many hours
+								of effort into the descriptions of these tasks.
+							</Text>
+						</GridItem>
+						<GridItem p={5} display="block">
+							<Stack direction="row" float="right">
+								<InsetButton>
+									<Icon as={FaEdit} boxSize={4} />
+								</InsetButton>
+								<InsetButton>
+									<Icon as={MdDelete} boxSize={4} />
+								</InsetButton>
+							</Stack>
+							<Heading size="md">Test item 1</Heading>
+							<Text>
+								This is a lengthy description that only exists
+								to take up an extremely unnecessary amount of
+								space and appear as though that I put many hours
+								of effort into the descriptions of these tasks.
+							</Text>
+						</GridItem>
+						<GridItem p={5} display="block">
+							<Stack direction="row" float="right">
+								<InsetButton>
+									<Icon as={FaEdit} boxSize={4} />
+								</InsetButton>
+								<InsetButton>
+									<Icon as={MdDelete} boxSize={4} />
+								</InsetButton>
+							</Stack>
+							<Heading size="md">Test item 1</Heading>
+							<Text>
+								This is a lengthy description that only exists
+								to take up an extremely unnecessary amount of
+								space and appear as though that I put many hours
+								of effort into the descriptions of these tasks.
+							</Text>
+						</GridItem>
+						<GridItem p={5} display="block">
+							<Stack direction="row" float="right">
+								<InsetButton>
+									<Icon as={FaEdit} boxSize={4} />
+								</InsetButton>
+								<InsetButton>
+									<Icon as={MdDelete} boxSize={4} />
+								</InsetButton>
+							</Stack>
+							<Heading size="md">Test item 1</Heading>
+							<Text>
+								This is a lengthy description that only exists
+								to take up an extremely unnecessary amount of
+								space and appear as though that I put many hours
+								of effort into the descriptions of these tasks.
+							</Text>
+						</GridItem>
+						<GridItem p={5} display="block">
+							<Stack direction="row" float="right">
+								<InsetButton>
+									<Icon as={FaEdit} boxSize={4} />
+								</InsetButton>
+								<InsetButton>
+									<Icon as={MdDelete} boxSize={4} />
+								</InsetButton>
+							</Stack>
+							<Heading size="md">Test item 1</Heading>
+							<Text>
+								This is a lengthy description that only exists
+								to take up an extremely unnecessary amount of
+								space and appear as though that I put many hours
+								of effort into the descriptions of these tasks.
+							</Text>
+						</GridItem>
 					</SimpleGrid>
 				</Box>
 				<GridItem as="button">
@@ -137,19 +214,30 @@ export default function Home() {
 	);
 }
 
-function GridItem({ children, ...props }: StackProps): JSX.Element {
+function GridItem(props: StackProps): JSX.Element {
 	return (
 		<Stack
 			rounded={30}
-			background="primary"
+			bg="primary"
 			boxShadow="15px 15px 30px #adc4cf, -15px -15px 30px #ebffff"
 			padding={2.5}
 			justify="center"
 			align="center"
 			direction="column"
 			{...props}
-		>
-			{children}
-		</Stack>
+		/>
+	);
+}
+
+function InsetButton(props: BoxProps): JSX.Element {
+	return (
+		<Center
+			rounded={30}
+			bg="primary"
+			boxShadow="inset 2px 2px 4px #adc4cf, inset -2px -2px 4px #ebffff"
+			padding={2.5}
+			as="button"
+			{...props}
+		/>
 	);
 }
